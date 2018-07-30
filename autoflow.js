3e04dc10400fcd2ca8cc25479809f56eedd6f6a4
Micah Raney <MicahRaney@users.noreply.github.com>
2018-07-30 00:27:16 -0700

    Updated autoflow to allow for multiple autoflows in a single html document
    Now to use autoflow, the class must be autoflow but the autoflowid
    attribute MUST also be specified. All autoflows of the same class will
    be set to the same height.

--- autoflow.js
+++ autoflow.js
@@ -2,22 +2,27 @@
 //Not to be used without permission
 function autoflow(){
 
-    var i;
-    var maxID;
+    var i, run = 0;
     var x = document.getElementsByClassName("autoflow");
-    var min = 0;
+    var min;
+    for(run = 0; true; run++){
+      min = 0;
+      for (i = 0; i < x.length; i++) {
+        if(x[i].getAttribute("autoflowid") == run)
+          x[i].style.height = "auto";
+      }
 
-    for (i = 0; i < x.length; i++) {
-       x[i].style.height = "auto";
-    }
-
-    for (i = 0; i < x.length; i++) {
-       if(x[i].scrollHeight > min){
-         min = x[i].scrollHeight;
-       }
-    }
-    for (i = 0; i < x.length; i++) {
-       x[i].style.height = min + "px";
+      for (i = 0; i < x.length; i++) {
+        if(x[i].getAttribute("autoflowid") == run)
+         if(x[i].scrollHeight > min)
+           min = x[i].scrollHeight;
+      }
+      for (i = 0; i < x.length; i++) {
+        if(x[i].getAttribute("autoflowid") == run)
+         x[i].style.height = min + "px";
+      }
+      if(min == 0)
+        break;
     }
 }
 window.onresize = autoflow;
