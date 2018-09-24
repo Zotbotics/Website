a41dd0d727b14d1316d0576fcfd999b9d1f0f2f7
Micah Raney <MicahRaney@users.noreply.github.com>
2018-09-23 17:40:21 -0700

    Added mailing list signup form

--- sign-up.php
+++ sign-up.php
@@ -3,9 +3,6 @@
 <title>Zotbotics | Sign Up</title>
 <?php include("common/header.html"); ?>
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <style>
 body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 body, html {
@@ -49,40 +46,36 @@ input:focus {
 .card{
   margin-left: 23%;
   width: 54%;
-  margin-right: 23%
-  background-color: #0064a4
+  margin-right: 23%;
+  /* background-color: #0064a4; */
 }
 .mc-field-group{
   margin-left: 10%;
   width: 80%;
-  margin-right: 10%
+  margin-right: 10%;
 }
-.birthday{
-  width: 10%
+/* .birthday{
+  width: 45px;
+  display: inline;
+} */
+.indicates-required{
+  float: right;
 }
+.w3-jumbo{
+  margin-bottom: 10px;
+}
+
+
 /* #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; } */
 /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
    We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 
 </style>
+</head>
+
 
 <?php include("common/navbar.html"); ?>
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-    <div class="col-md-1">.col-md-1</div>
-  </div>
-</div>
+
 <!-- Header with full-height image -->
 <div class="bgimg-summer w3-display-container w3-center w3-padding-64" id="home">
   <!-- <div class="w3-display-middle" style="width:90%">
@@ -118,10 +111,10 @@ input:focus {
   <div class="w3-display-middle" style="width:90%">
     <p class="w3-jumbo">Sign up for our newsletter!</p>
     <div id="mc_embed_signup">
-      <div class="w3-card-4 w3-sand card">
+      <div class="w3-panel w3-card-4 card">
         <form action="https://zotbotics.us18.list-manage.com/subscribe/post?u=8193a74c89f2c1415b6168cb8&amp;id=3a7b6dadfd" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
           <div id="mc_embed_signup_scroll">
-            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div><br>
               <div class="mc-field-group">
               	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
                 </label>
@@ -135,28 +128,20 @@ input:focus {
             	<label for="mce-LNAME">Last Name </label>
             	<input type="text" value="" name="LNAME" class="w3-input w3-border w3-hover-border-gray" id="mce-LNAME">
             </div>
-            <div class="mc-field-group size1of2">
-            	<label for="mce-BIRTHDAY-month">Birthday </label>
-            	<div class="datefield row">
-                <div><span class="subfield monthfield"><input class="md-col-1 w3-input w3-border w3-hover-border-gray birthday " type="text" pattern="[0-9]*" value="" placeholder="MM" size="2" maxlength="2" name="BIRTHDAY[month]" id="mce-BIRTHDAY-month"></span></div> /
-            		<div><span class="subfield dayfield"><input class="md-col-1 w3-input w3-border w3-hover-border-gray birthday " type="text" pattern="[0-9]*" value="" placeholder="DD" size="2" maxlength="2" name="BIRTHDAY[day]" id="mce-BIRTHDAY-day"></span></div>
-            		<span class="small-meta nowrap">( mm / dd )</span>
-            	</div>
+            <div class="mc-field-group">
+              <label for="mce-PHONE">Phone Number </label>
+              <input type="tel" value="" name="PHONE" class="w3-input w3-border w3-hover-border-gray" id="mce-PHONE">
             </div>
             <div class="mc-field-group">
             	<label for="mce-MMERGE6">Current Year </label>
             	<input type="text" value="" name="MMERGE6" class="w3-input w3-border w3-hover-border-gray" id="mce-MMERGE6">
-            </div>
-            <div class="mc-field-group">
-            	<label for="mce-MMERGE7">Year </label>
-            	<input type="text" value="" name="MMERGE7" class="w3-input w3-border w3-hover-border-gray" id="mce-MMERGE7">
             </div>
           	<div id="mce-responses" class="clear">
           		<div class="response" id="mce-error-response" style="display:none"></div>
           		<div class="response" id="mce-success-response" style="display:none"></div>
           	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
             <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8193a74c89f2c1415b6168cb8_3a7b6dadfd" tabindex="-1" value=""></div>
-            <div class="clear"><br><button class="w3-button w3-black" type="submit">
+            <div class="clear"><br><button class="w3-button w3-text-color-white w3-color-grey" type="submit">
                 <i class="fa fa-paper-plane"></i> SEND MESSAGE
               </button>
             </div>
