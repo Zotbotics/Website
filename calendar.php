c64e386c2a828577b6b11098b81efb56cd3d471e
Micah Raney <MicahRaney@users.noreply.github.com>
2018-10-22 22:33:52 -0700

    Added calendar integration

---
+++ calendar.php
@@ -0,0 +1,35 @@
+<html>
+<title>Zotbotics | Calendar</title>
+<head>
+<?php include('common/header.html');?>
+</head>
+
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+body, html {
+    height: 100%;
+    line-height: 1.8;
+}
+.w3-bar .w3-button {
+    padding: 16px;
+}
+input:invalid {
+    border: 2px solid red;
+}
+</style>
+
+<?php include('common/navbar.html');?>
+
+
+<div class="w3-container w3-content w3-center w3-padding-64">
+<h2 class="w3-wide">CLUB CALENDAR</h2>
+<br>
+<iframe src="https://calendar.google.com/calendar/embed?src=i4mj1jrm10q7900u3od2qfaaeg%40group.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+</div>
+<!-- Footer -->
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-16">
+  <?php include("common/legal.html"); ?>
+</footer>
+</html>
